88c631597f853e93cf4538074aaede03584d679b
redplug <blies@naver.com>
2020-07-31 18:38:35 +0900

    Update ADjoinScript.ps1

--- ADJoinScript/ADjoinScript.ps1
+++ ADJoinScript/ADjoinScript.ps1
@@ -1,16 +1,16 @@
 Read-Host "도메인 조인 작업을 시작 합니다. `n`재부팅이 진행되오니 필요한 파일 저장 부탁드립니다 `n`시작하시려면 아무키나 눌러주세요"
 
-### Yauser 로컬 관리자 계정 설정 ###
-$YauserPassword = ConvertTo-SecureString "yayaya@123!" –AsPlainText –Force
-$yauserexist = get-localuser -name  "yauser"
+### reuser 로컬 관리자 계정 설정 ###
+$reuserPassword = ConvertTo-SecureString "rerere@123!" –AsPlainText –Force
+$reuserexist = get-localuser -name  "reuser"
 
-if ($yauserexist -ne $null) {
-    ## Yauser가 존재할 경우 패스워드만 초기화
-    set-LocalUser -Name yauser -Password $YauserPassword
+if ($reuserexist -ne $null) {
+    ## reuser가 존재할 경우 패스워드만 초기화
+    set-LocalUser -Name reuser -Password $reuserPassword
 }else{
-    ## Yauser가 존재하지 않을 경우 계정 생성 및 초기화
-    New-LocalUser -Name yauser -Password $YauserPassword -AccountNeverExpires -FullName yauser -PasswordNeverExpires
-    Add-LocalGroupMember -Group Administrators -Member yauser
+    ## reuser가 존재하지 않을 경우 계정 생성 및 초기화
+    New-LocalUser -Name reuser -Password $reuserPassword -AccountNeverExpires -FullName reuser -PasswordNeverExpires
+    Add-LocalGroupMember -Group Administrators -Member reuser
 }
 
 ### 도메인 조인 ###
@@ -29,42 +29,26 @@ if ($company -eq 4){
     exit
 }
 
-##도메인 조인
+### 도메인 조인 ###
+
 $domain = "redplug.test"
-$account = read-host "AD계정명을 입력부탁드립니다."
-## 계정에 점있으면 제거
+$account = read-host "AD계정명을 입력부탁드립니다." 
 $accountnondot = $account -replace ('\.','')
 
 switch($company){
-    1 { ## A법인
-        $ComputerTrue = get-adcomputer -Identity A-$accountnondot
-        if($ComputerTrue -eq $null){
-            Add-Computer -DomainName $Domain -NewName A-$accountnondot
-       } else {
-            $ComputerName = Read-host "기존 컴퓨터 이름이 존재합니다. 수동으로 입력 부탁드립니다."
-            Add-Computer -DomainName $Domain -NewName $ComputerName
-       }
-       }
-    2 { ## B법인
-        $ComputerTrue = get-adcomputer -Identity B-$accountnondot
-        if($ComputerTrue -eq $null){
-            Add-Computer -DomainName $Domain -NewName B-$accountnondot
-       } else {
-            $ComputerName = Read-host "기존 컴퓨터 이름이 존재합니다. 수동으로 입력 부탁드립니다."
-            Add-Computer -DomainName $Domain -NewName $ComputerName
-       }
-      }
-    3 { ## C법인
-        $ComputerTrue = get-adcomputer -Identity C-$accountnondot
-        if($ComputerTrue -eq $null){
-            Add-Computer -DomainName $Domain -NewName C-$accountnondot
-       } else {
-            $ComputerName = Read-host "기존 컴퓨터 이름이 존재합니다. 수동으로 입력 부탁드립니다."
-            Add-Computer -DomainName $Domain -NewName $ComputerName
-       }       
-      }
+    1 { $Hostname = "A-$accountnondot"}
+    2 { $Hostname = "B-$accountnondot"}
+    3 { $Hostname = "C-$accountnondot"}
 }
 
+$ComputerTrue = get-adcomputer -Identity $Hostname
+
+if($ComputerTrue -eq $null){
+    Add-Computer -DomainName $Domain -NewName $Hostname
+    } else {
+        $AnotherHostname = Read-host "기존 컴퓨터 이름($Hostname)이 존재합니다. 수동으로 입력 부탁드립니다."
+        Add-Computer -DomainName $Domain -NewName $AnotherHostname
+    }
 sleep 10;
 
 ## 사용자 계정 관리자 그룹 추가
@@ -72,3 +56,5 @@ Add-LocalGroupMember -Group Administrators -Member redplug\$account
 
 Read-Host "아무키나 눌러주시면 컴퓨터를 재시작 합니다."
 Restart-Computer -Force
+
+
