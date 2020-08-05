508edac43898350b7af2017a2c715c6fe4024411
redplug <blies@naver.com>
2020-08-05 15:07:33 +0900

    Update join_run.bat

--- ADJoinScript/join_run.bat
+++ ADJoinScript/join_run.bat
@@ -1,5 +1,5 @@
 @echo off
 mkdir c:\join
-copy * c:\join
+xcopy * c:\join /s /y /h
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""C:\join\ADjoinscripts.ps1""' -Verb RunAs}" 
 pause
